2be773e3b5c30afa47ba63d23aaec56eadcc3c55
cesvh <cesar.vega.hdez@gmail.com>
2025-04-16 15:18:00 -0600

    feat: asp.net fram4 - Se agrega EF
    Se agrega módelo de db con entity framework

--- 001-web/001-web/001-net-framework/001-CNetFrame4MVCEntFram/Controllers/AccessController.cs
+++ 001-web/001-web/001-net-framework/001-CNetFrame4MVCEntFram/Controllers/AccessController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _001_CNetFrame4MVCEntFram.Models;
 
 namespace _001_CNetFrame4MVCEntFram.Controllers
 {
@@ -18,6 +20,16 @@ namespace _001_CNetFrame4MVCEntFram.Controllers
         {
             try
             {
+                using (t_cmvcEntities db = new t_cmvcEntities())
+                {
+                    var lst = from d in db.tb_users
+                               where d.email == user && d.password == password && d.idState == 1
+                               select d;
+                    if (lst.Count() > 0)
+                    {
+                        Session["user"] = lst.First();
+                    }
+                }
                 return Json(new { user, password });
             }
             catch (Exception ex)
