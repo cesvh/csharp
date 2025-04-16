1d84c66a0afb8645fc9482cd2d06f16462da7313
cesvh <cesar.vega.hdez@gmail.com>
2025-04-16 00:17:04 -0600

    feat: asp.net fram 4 - Se agrega controlador y vista de Access

---
+++ 001-web/001-web/001-net-framework/001-CNetFrame4MVCEntFram/Controllers/AccessController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _001_CNetFrame4MVCEntFram.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(string user, string password)
+        {
+            try
+            {
+                return Json(new { user, password });
+            }
+            catch (Exception ex)
+            {
+                return Content("Ocurrió un error: " + ex.Message);
+            }
+        }
+    }
+}
