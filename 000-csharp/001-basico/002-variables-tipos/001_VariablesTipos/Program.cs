9f7d72dad80ec0026bd19be6042c4a6f3adb3e75
cesvh <cesar.vega.hdez@gmail.com>
2025-04-25 00:07:03 -0600

    feat: c# Variables y Tipos

---
+++ 000-csharp/001-basico/002-variables-tipos/001_VariablesTipos/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+
+int myNumber = 299;
+string myName = "César";
+
+WriteLine($"Mi número es {myNumber} y mi nombre es {myName}");
---
+++ 000-csharp/001-basico/002-variables-tipos/001_VariablesTipos/___logs.sh
@@ -0,0 +1,37 @@
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos> dotnet new console -o 001_VariablesTipos
+La plantilla "Aplicación de consola" se creó correctamente.
+
+Procesando acciones posteriores a la creación...
+Restaurando C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\001_VariablesTipos\001_VariablesTipos.csproj:
+Restauración realizada correctamente.
+
+
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos> cd .\001_VariablesTipos\
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\001_VariablesTipos>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\001_VariablesTipos>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\001_VariablesTipos> ls
+
+    Directory: C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\001_VariablesTipos
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----       24/04/2025 11:52 p. m.                obj
+-a---       24/04/2025 11:52 p. m.            308 001_VariablesTipos.csproj
+-a---       24/04/2025 11:52 p. m.            105 Program.cs
+
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\001_VariablesTipos>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\001_VariablesTipos>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\001_VariablesTipos> dotnet run
+Mi número es 299 y mi nombre es César
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\001_VariablesTipos>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\001_VariablesTipos>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\001_VariablesTipos> dotnet build
+Restauración completada (0.4s)
+  001_VariablesTipos realizado correctamente (0.2s) → bin\Debug\net9.0\001_VariablesTipos.dll
+
+Compilación realizado correctamente en 1.2s
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\001_VariablesTipos>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\001_VariablesTipos>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\001_VariablesTipos>
