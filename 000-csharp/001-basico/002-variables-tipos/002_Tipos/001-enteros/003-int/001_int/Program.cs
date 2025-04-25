b5198fb972081b125dbb2cc10a5f4e700c21fc3f
cesvh <cesar.vega.hdez@gmail.com>
2025-04-25 00:49:41 -0600

    feat: c# int

---
+++ 000-csharp/001-basico/002-variables-tipos/002_Tipos/001-enteros/003-int/001_int/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+
+// int: Entero de 32 bits con signo, que puede almacenar valores entre -2,147,483,648 y 2,147,483,647.
+int myNumber = 299;
+WriteLine($"Mi número es {myNumber}");
+WriteLine($"int range: ({int.MinValue}, {int.MaxValue})");
---
+++ 000-csharp/001-basico/002-variables-tipos/002_Tipos/001-enteros/003-int/001_int/___logs.sh
@@ -0,0 +1,26 @@
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\001-enteros\003-int>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\001-enteros\003-int> dotnet new console -o 001_int
+La plantilla "Aplicación de consola" se creó correctamente.
+
+Procesando acciones posteriores a la creación...
+Restaurando C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\001-enteros\003-int\001_int\001_int.csproj:
+Restauración realizada correctamente.
+
+
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\001-enteros\003-int>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\001-enteros\003-int>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\001-enteros\003-int> cd .\001_int\
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\001-enteros\003-int\001_int>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\001-enteros\003-int\001_int> dotnet run
+Mi número es 299
+int range: (-2147483648, 2147483647)
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\001-enteros\003-int\001_int>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\001-enteros\003-int\001_int>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\001-enteros\003-int\001_int> dotnet build
+Restauración completada (0.4s)
+  001_int realizado correctamente (0.2s) → bin\Debug\net9.0\001_int.dll
+
+Compilación realizado correctamente en 1.2s
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\001-enteros\003-int\001_int>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\001-enteros\003-int\001_int>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\001-enteros\003-int\001_int>
