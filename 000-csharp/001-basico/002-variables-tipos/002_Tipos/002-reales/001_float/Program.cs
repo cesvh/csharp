a53874c7b5db541412f60086b5475bdd8a5fde90
cesvh <cesar.vega.hdez@gmail.com>
2025-04-25 15:28:02 -0600

    feat: c# float

---
+++ 000-csharp/001-basico/002-variables-tipos/002_Tipos/002-reales/001_float/Program.cs
@@ -0,0 +1,6 @@
+﻿using static System.Console;
+
+float myFloat = 299_792.458f;
+
+WriteLine($"Mi número float es: {myFloat}");
+
---
+++ 000-csharp/001-basico/002-variables-tipos/002_Tipos/002-reales/001_float/___logs.sh
@@ -0,0 +1,24 @@
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\002-reales>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\002-reales> dotnet new console -o 001_float
+La plantilla "Aplicación de consola" se creó correctamente.
+
+Procesando acciones posteriores a la creación...
+Restaurando C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\002-reales\001_float\001_float.csproj:
+Restauración realizada correctamente.
+
+
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\002-reales>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\002-reales>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\002-reales> cd .\001_float\
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\002-reales\001_float>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\002-reales\001_float>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\002-reales\001_float> dotnet run
+Mi número float es: 299792.47
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\002-reales\001_float>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\002-reales\001_float> dotnet build
+Restauración completada (0.3s)
+  001_float realizado correctamente (0.2s) → bin\Debug\net9.0\001_float.dll
+
+Compilación realizado correctamente en 1.0s
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\002-reales\001_float>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\002-reales\001_float>
