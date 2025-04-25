595914febdac8f45b4c977d3c789ca9128a49166
cesvh <cesar.vega.hdez@gmail.com>
2025-04-25 15:42:28 -0600

    feat: c# char

---
+++ 000-csharp/001-basico/002-variables-tipos/002_Tipos/003-char/001_char/Program.cs
@@ -0,0 +1,4 @@
+﻿using static System.Console;
+
+char myChar = 'l';
+WriteLine($"Mi variable char es: {myChar}");
---
+++ 000-csharp/001-basico/002-variables-tipos/002_Tipos/003-char/001_char/___logs.sh
@@ -0,0 +1,35 @@
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char> dotnet new console -o 001_char
+La plantilla "Aplicación de consola" se creó correctamente.
+
+Procesando acciones posteriores a la creación...
+Restaurando C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char\001_char\001_char.csproj:
+Restauración realizada correctamente.
+
+
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char> cd .\001_char\
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char\001_char>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char\001_char>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char\001_char> ls
+
+    Directory: C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char\001_char
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d----       25/04/2025 03:35 p. m.                obj
+-a---       25/04/2025 03:35 p. m.            298 001_char.csproj
+-a---       25/04/2025 03:35 p. m.            105 Program.cs
+
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char\001_char>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char\001_char> dotnet run
+Mi variable char es: l
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char\001_char>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char\001_char> dotnet build
+Restauración completada (0.3s)
+  001_char realizado correctamente (0.2s) → bin\Debug\net9.0\001_char.dll
+
+Compilación realizado correctamente en 1.2s
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char\001_char>
+PS C:\Users\cesvh\dev\github\csharp\000-csharp\001-basico\002-variables-tipos\002_Tipos\003-char\001_char>
