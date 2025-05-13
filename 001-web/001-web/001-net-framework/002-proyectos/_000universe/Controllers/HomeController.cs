e0cb4b5443a96aa2555cd741e5006353429af316
cesvh <cesar.vega.hdez@gmail.com>
2025-05-13 16:28:18 -0600

    feat: C#-ASP.NET .NET FRAMEWORK, Vistas, práctiva y teoría

--- 001-web/001-web/001-net-framework/002-proyectos/_000universe/Controllers/HomeController.cs
+++ 001-web/001-web/001-net-framework/002-proyectos/_000universe/Controllers/HomeController.cs
@@ -11,9 +11,9 @@ namespace _000universe.Controllers
         // GET: Home
         // https://localhost:44334/home/index: home -> index: / -> route default
         // En el archivo de clase RouteConfig.cs y verá que el método RegisterRoutes() de la clase RouteConfig tiene una ruta predeterminada
-        public string Index()
+        public ActionResult Index()
         {
-            return "Hello MVC 5 Application";
+            return View();
         }
 
         // https://localhost:44334/home/indexid/29
@@ -25,11 +25,21 @@ namespace _000universe.Controllers
         // https://localhost:44334/home/indexidstring/29?username=cesar
         public string IndexIdString(int id, string username) {
             return $"El valor del id es: {id} y del username es: {username}";
+            //return "Id = " + id + " ,Name = " + Request.QueryString["username"]; // Otra opción de obtener el parámetro "username" del query
         }
 
         // La clase controlador contiene un conjunto de métodos públicos, también llamados métodos de acción. Estos métodos de acción gestionan las solicitudes HTTP entrantes.
         // En ASP.NET MVC, el nombre de cada clase de controlador debe terminar con la palabra "Controller".
         // Por ejemplo, si tiene una clase llamada "HomeController", el nombre del controlador en la URL será "Home".
         // Cada clase de controlador debe estar ubicada en la carpeta Controladores.
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs() {
+            return View();
+        }
     }
 }
