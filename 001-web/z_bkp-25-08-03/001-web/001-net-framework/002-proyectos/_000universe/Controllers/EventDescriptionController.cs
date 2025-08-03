c356c0cb7819aa575ac84d1dbfb7ad6e713ab0b1
cesvh <cesar.vega.hdez@gmail.com>
2025-08-03 17:27:10 -0600

    fix: corrección de topics y de error, se agrega cambio en .gitignore

---
+++ 001-web/z_bkp-25-08-03/001-web/001-net-framework/002-proyectos/_000universe/Controllers/EventDescriptionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _000universe.Models.BusinessLayer;
+using _000universe.Models.ViewModels;
+
+namespace _000universe.Controllers
+{
+    public class EventDescriptionController : Controller
+    {
+        // GET: EventDescription
+        public ActionResult Index()
+        {
+            EventDescriptionBusinessLayer eventDescriptionBL = new EventDescriptionBusinessLayer();
+            EventDescriptionViewModel eventDescriptionVM = eventDescriptionBL.GetEventDescriptionById(1);
+
+            return View(eventDescriptionVM);
+        }
+
+        // url: "EventDescription/Secreto". En App_Start/RouteConfig.cs
+        public string CustomRoute() {
+            return "Método/Acción de la ruta personalizada";
+        }
+    }
+}
--- 003-testing/002-web/001-api/000_test/_001WebApi/Models/Eventos.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _001WebApi.Models
-{
-    public class Eventos
-    {
-        public int Id { get; set; }
-        public string Evento { get; set; }
-        public string Descripcion { get; set; }
-    }
-}
